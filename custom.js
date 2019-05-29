d647181fa6139018db7c8abaf27446710b2798d6
s30408 <45545124+s30408@users.noreply.github.com>
2019-05-29 19:46:00 +0900

    ver.1

---
+++ custom.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function(){
+
+	//페이지 로딩 시 제일 상단으로 스크롤 이동
+	$("body,html").stop().animate({"scrollTop":0},1500,"swing");
+
+	$(window).on("scroll",function(){
+
+		//변수 scroll에 현재 화면을 스크롤한 거리의 수치를 저장
+		var scroll = $(this).scrollTop();
+
+		for(var i=0; i<10; i++){
+			//스크롤값과 박스의 z축 연동
+			$("section>article").eq(i).css({"transform":"translateZ("   + (-5000*i+scroll)   +  "px)"});
+			//특정 스크롤 구간에서 스크롤 네비게이션과 박스 활성화
+			if(scroll>=i*5000-2500&&scroll<(i+1)*5000-2500){
+				$(".scrollNavi li").removeClass();
+				$(".scrollNavi li").eq(i).addClass("on");
+				$("article").removeClass();
+				$("article").eq(i).addClass("on");
+			};
+		};
+
+	});
+
+
+	//스크롤 네비게이션 클릭 시 스크롤 이동
+	$(".scrollNavi li").on("click",function(){
+		var a = $(this).index();
+		$("body,html").stop().animate({"scrollTop":5000*a},1500,"swing");
+	});
+
+
+
+});
